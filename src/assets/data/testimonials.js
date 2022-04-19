42304377848712d15744f85d5d23ab8529bc9242
Andréas Ducourneau <andreas.ducourneau@viacesi.fr>
2022-04-19 17:04:54 +0200

    Adding specification on myself

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -5,7 +5,7 @@ const testimonials = [
     title: 'CEO',
     org: 'WEB CIFAR',
     desc:
-      'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+      'Person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
   },
   {
     id: 2,
